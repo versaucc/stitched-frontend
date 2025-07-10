5fa3bd5a6245c30c379f1ae58338ce57a594dd18
Oliver <oliisbali@gmail.com>
2025-07-09 23:06:42 -0700

    FIX: Rm supabase middleware

--- app/middleware/middleware.tsx
+++
@@ -1,7 +0,0 @@
-import { withMiddlewareAuth } from '@supabase/auth-helpers-nextjs';
-
-export const middleware = withMiddlewareAuth();
-
-export const config = {
-  matcher: ['/admin', '/dashboard'], // secure routes
-};
