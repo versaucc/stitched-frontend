63db6a48a19322a15d8f008640bfda5f21c271fd
Oliver <oliisbali@gmail.com>
2025-07-09 22:31:53 -0700

    FIX: missing return for orders page

---
+++ app/account/orders/page.tsx
@@ -0,0 +1,8 @@
+export default function OrdersPage() {
+  return (
+    <div className="p-4 text-white">
+      <h1 className="text-2xl font-bold">Your Orders</h1>
+      {/* You can fill this out later */}
+    </div>
+  )
+}
