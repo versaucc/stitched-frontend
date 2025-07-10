c680e2890f34e33ecca11f93a4ec0b14000312e1
Oliver <oliisbali@gmail.com>
2025-07-09 23:58:07 -0700

    FIX: Switch page.js -> page.jsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import AnimatedLanding from '../components/effects/AnimatedLanding';
+
+export default function Home() {
+  return (
+    <div>
+      <AnimatedLanding/>
+    </div>
+  );
+}
