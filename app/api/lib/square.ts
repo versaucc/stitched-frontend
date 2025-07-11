4add81db1399318d8d17611fe4ddf61fb9abdb52
Oliver <oliisbali@gmail.com>
2025-07-10 22:25:55 -0700

    FIX: Import error

--- app/api/lib/square.ts
+++ app/api/lib/square.ts
@@ -1,8 +1,8 @@
 // lib/square.ts
-import { Client, Environment } from 'square';
+import { SquareClient } from 'square';
 
-export const squareClient = new Client({
-  accessToken: process.env.SQUARE_ACCESS_TOKEN!,
-  environment: process.env.NODE_ENV === 'production' ? Environment.Production : Environment.Sandbox,
+export const squareClient = new SquareClient({
+  token: process.env.SQUARE_ACCESS_TOKEN!,
+  environment: 'production',
 });
 
