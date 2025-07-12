76f5dddda365909b38c357aa4f14d940a4eafee8
Oliver <oliisbali@gmail.com>
2025-07-12 03:34:56 -0700

    add className support

--- components/fred/FredContainer.tsx
+++ components/fred/FredContainer.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { ReactNode } from 'react'
 
 export default function FredContainer({
@@ -7,31 +5,20 @@ export default function FredContainer({
   flagA,
   flagB,
   children,
+  className = '',
 }: {
   message: string
   flagA: boolean
   flagB: boolean
-  children: ReactNode
+  children?: ReactNode
+  className?: string
 }) {
   return (
-    <div className="relative">
-      {/* Fred + Message Box */}
-      <div className="absolute top-1/2 left-[10%] transform -translate-y-1/2 z-10">
-        <img
-          src="/fred/fred-wave.png"
-          alt="Fred"
-          className="w-56 h-auto"
-        />
-        <div className="bg-white text-black p-3 rounded shadow-md mt-2 max-w-xs text-sm">
-          {message}
-        </div>
-      </div>
-
-      {/* Content of the walkthrough step */}
-      <div className="ml-[160px]"> {/* leave space for Fred */}
-        {children}
-      </div>
+    <div className={`text-white p-4 rounded shadow ${className}`}>
+      <p>{message}</p>
+      {children}
     </div>
   )
 }
+
 
