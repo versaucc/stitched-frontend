e64593b2957c3b6909712056a8a075f62e846f5d
Oliver <oliisbali@gmail.com>
2025-07-12 03:39:08 -0700

    Updated for img support

--- components/fred/FredContainer.tsx
+++ components/fred/FredContainer.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { ReactNode } from 'react'
 
 export default function FredContainer({
@@ -10,13 +12,27 @@ export default function FredContainer({
   message: string
   flagA: boolean
   flagB: boolean
-  children?: ReactNode
+  children: ReactNode
   className?: string
 }) {
   return (
-    <div className={`text-white p-4 rounded shadow ${className}`}>
-      <p>{message}</p>
-      {children}
+    <div className={`relative ${className}`}>
+      {/* Fred + Message Box */}
+      <div className="absolute top-3/4 left-[10%] transform -translate-y-1/4 z-10">
+        <img
+          src="/fred/fred-wave.png"
+          alt="Fred"
+          className="w-56 h-auto"
+        />
+        <div className="bg-white text-black p-3 rounded shadow-md mt-2 max-w-xs text-sm">
+          {message}
+        </div>
+      </div>
+
+      {/* Content of the walkthrough step */}
+      <div className="ml-[160px]">
+        {children}
+      </div>
     </div>
   )
 }
