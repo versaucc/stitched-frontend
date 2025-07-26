c15dcc6d979f5f4501b67c2e60b833b57ed5bd00
Oliver <oliisbali@gmail.com>
2025-07-26 15:52:22 -0700

    remove api functions for time being

--- app/api/lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://cikoufovmpryfwinwugo.supabase.co',
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
